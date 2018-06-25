435aebcc30abcfb15f8e6e6c26c226c46158bdd8
Yash Vesikar <yashvesikar@gmail.com>
2018-06-25 17:41:51 -0400

    Modified ERNGSA2 normalization as per Dr.Deb's suggestions

--- pymoo/algorithms/ERNSGAII.py
+++ pymoo/algorithms/ERNSGAII.py
@@ -1,16 +1,23 @@
 from pymoo.algorithms.genetic_algorithm import GeneticAlgorithm
 from pymoo.operators.default_operators import set_default_if_none, set_if_none
-from pymoo.operators.survival.rnsga_reference_line_survival import ReferenceLineSurvival
+from pymoo.operators.survival.reference_point_survival import ReferencePointSurvival
 import numpy as np
 
 class ERNSGAII(GeneticAlgorithm):
     """
     Class Extended RNSGA-II
     """
-    def __init__(self, var_type, ref_points=None, epsilon=0.001, weights=None, **kwargs):
+    def __init__(self,
+                 var_type,
+                 ref_points=None,
+                 epsilon=0.001,
+                 weights=None,
+                 normalize=True,
+                 **kwargs):
         self.ref_dirs = np.array(ref_points)
         self.epsilon = epsilon
         self.weights = weights
+        self.normalize = normalize
         set_default_if_none(var_type, kwargs)
         set_if_none(kwargs, 'survival', None)
         super().__init__(**kwargs)
@@ -18,8 +25,9 @@ class ERNSGAII(GeneticAlgorithm):
     def _initialize(self, problem):
         super()._initialize(problem)
 
+
         # if survival not define differently
         if self.survival is None:
 
             # set the survival method itself
-            self.survival = ReferenceLineSurvival(self.ref_dirs, self.epsilon, self.weights)
+            self.survival = ReferencePointSurvival(self.ref_dirs, self.epsilon, self.weights, problem.n_obj)
--- pymoo/util/reference_directions.py
+++ pymoo/util/reference_directions.py
@@ -52,7 +52,7 @@ def get_ref_dirs_from_points(points, n_obj):
     ref_points = np.append(points, extreme_reference_points, axis=0)
     return ref_points
 
-def get_ref_dirs_from_points(ref_point, n_obj, alpha=0.1):
+def get_ref_dirs_from_points(ref_point, n_obj, pop_size, alpha=0.1, method='uniform'):
     """
     This function takes user specified reference points, and creates smaller sets of equidistant
     Das-Dennis points around the projection of user points on the Das-Dennis hyperplane
@@ -64,30 +64,49 @@ def get_ref_dirs_from_points(ref_point, n_obj, alpha=0.1):
     ref_dirs = []
     n_vector = np.ones(n_obj) / np.linalg.norm(np.ones(n_obj))  # Normal vector of Das Dennis plane
     point_on_plane = np.eye(n_obj)[0]  # Point on Das-Dennis
-    reference_directions = get_ref_dirs_from_n(n_obj, 21)  # Das-Dennis points
+    # point_on_plane = np.array([0.333333]*n_obj)
+    pop_size = pop_size/len(ref_point)  # Limit the number of Das-Dennis points
+    if method == 'uniform':
+        reference_directions = get_ref_dirs_from_n(n_obj, pop_size)  # Das-Dennis points
+    elif method == 'random':
+        reference_directions = np.random.rand(int(pop_size), int(n_obj))
+        reference_directions = reference_directions - np.dot(reference_directions - point_on_plane, n_vector)[:, None] * n_vector
+    elif method =='nested':
+        reference_directions = get_ref_dirs_from_section(n_obj, 3)  # Das-Dennis points
+        # ref_dirs.extend(reference_directions)
+        nested = get_ref_dirs_from_section(n_obj, 2)
+        nested = 0.3333333 + 0.5 * (nested - 0.3333333) # Shrink the nested region
+        nested = (nested - np.dot(nested - point_on_plane, n_vector)[:, None] * n_vector)
+        reference_directions = np.vstack((reference_directions, nested))
+        # return np.array(ref_dirs)
 
     for point in ref_point:
-        # ref_proj = point - np.dot(point - point_on_plane, n_vector) * n_vector
+        ref_proj = point - np.dot(point - point_on_plane, n_vector) * n_vector
         # TODO: Compute which is faster, a copy.deepcopy, or recomputing all the points from get_ref_dirs_from_n
         ref_dir = copy.deepcopy(reference_directions)  # Copy of computed reference directions
         for i in range(n_obj):  # Shrink Das-Dennis points by a factor of alpha
-            ref_dir[:, i] = point[i] + alpha * (ref_dir[:, i] - point[i])
+            # ref_dir[:, i] = point[i] + alpha * (ref_dir[:, i] - point[i])
+            ref_dir[:, i] = ref_proj[i] + alpha * (ref_dir[:, i] - ref_proj[i])
         for d in ref_dir:  # Project shrunked Das-Dennis points back onto original Das-Dennis hyperplane
             ref_dirs.append(d - np.dot(d - point_on_plane, n_vector) * n_vector)
     # TODO: Extreme points are only extreme of the scale is normalized between 0-1, how to make them truly extreme?
     ref_dirs.extend(np.eye(n_obj))  # Add extreme points
+    # extreme = get_ref_dirs_from_n(n_obj, 24)
+    # ref_dirs.extend(extreme)
     return np.array(ref_dirs)
+
+def norm(F):
+    F_min = F.min(axis=0)
+    F_max = F.max(axis=0)
 
 
 if __name__ == '__main__':
 
-    test = get_ref_dirs_from_n(2, 100)
+    w = get_ref_dirs_from_points(np.array([[0.2, 0.6, 0.8]]), n_obj=3, pop_size=100, method='nested')
+    import matplotlib.pyplot as plt
 
-    # for i in [3]:
-    #     for j in range(20):
-    #         test = get_ref_dirs_from_section(i, j)
-    #         print(j, len(test), get_number_of_reference_directions(i, j))
-    # print()
-    import pylab as pl
-    fig = pl.subplot()
-    pl.scatter(test[:, 0], test[:, 1])
+    fig = plt.figure()
+    from mpl_toolkits.mplot3d import Axes3D
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(w[:, 0], w[:, 1], w[:, 2])
+    plt.show()
