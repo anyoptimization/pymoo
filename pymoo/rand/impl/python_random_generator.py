ac0da1d84c37820b9cde81edec2cf5cf80b4f1f9
Julian <jules89@arcor.de>
2018-08-21 11:18:58 -0400

    Improved version of NSGA2 and NSGA3

---
+++ pymoo/rand/impl/python_random_generator.py
@@ -0,0 +1,16 @@
+import random
+
+from pymoo.rand.random_generator import RandomGenerator
+
+
+class PythonRandomGenerator(RandomGenerator):
+
+    def _seed(self, n):
+        random.seed(n)
+
+    def _rand_float(self):
+        return random.random()
+
+
+
+
--- pymoo/rand/impl/secure_random_generator.py
+++ pymoo/rand/impl/secure_random_generator.py
@@ -1,27 +1,16 @@
-
 import random
-import numpy as np
 
-class SecureRandomGenerator:
-
-    def random(self, size=None):
-        if size is None:
-            return random.SystemRandom().random()
-        else:
-            n = size[0]
-            m = size[1]
-            val = np.zeros((n, m))
-            for i in range(n):
-                for j in range(m):
-                    val[i, j] = random.SystemRandom().random()
-
-            return val
+from pymoo.rand.random_generator import RandomGenerator
 
 
-    def permutation(self, n):
-        perm = list(range(n))
-        random.SystemRandom().shuffle(perm)
-        return perm
+class DefaultRandomGenerator(RandomGenerator):
 
-    def seed(self, x):
-        pass
+    def _seed(self, n):
+        random.seed(n)
+
+    def _rand_float(self):
+        return random.random()
+
+
+
+
