7ca98e2f5bdbaefaed3de7f498444e10f9c3b27d
Julian <jules89@arcor.de>
2018-08-29 15:57:15 -0400

    Termination Criterium Added

--- pymoo/util/display.py
+++ pymoo/util/display.py
@@ -5,12 +5,11 @@ from pymoo.indicators.igd import IGD
 
 
 def disp_single_objective(problem, evaluator, D):
-    attrs = [('n_gen', D['n_gen']+1, 5),
+    attrs = [('n_gen', D['n_gen'], 5),
              ('n_eval', evaluator.n_eval, 7)]
 
     if problem.n_constr > 0:
-        attrs.append(('min_constr', "%.5f" % np.min(D['pop'].CV), 8))
-        attrs.append(('avg_constr', "%.5f" % np.mean(D['pop'].CV), 8))
+        attrs.append(('cv (min/avg)', "%.5f / %.5f" % (np.min(D['pop'].CV), np.mean(D['pop'].CV)), 13))
 
     attrs.extend([
              ('favg', "%.5f" % np.mean(D['pop'].F), 5),
@@ -21,12 +20,11 @@ def disp_single_objective(problem, evaluator, D):
 
 
 def disp_multi_objective(problem, evaluator, D):
-    attrs = [('n_gen', D['n_gen']+1, 5),
+    attrs = [('n_gen', D['n_gen'], 5),
              ('n_eval', evaluator.n_eval, 7)]
 
     if problem.n_constr > 0:
-        attrs.append(('min_constr', "%.5f" % np.min(D['pop'].CV), 8))
-        attrs.append(('avg_constr', "%.5f" % np.mean(D['pop'].CV), 8))
+        attrs.append(('cv (min/avg)', "%.5f / %.5f" % (np.min(D['pop'].CV), np.mean(D['pop'].CV)), 13))
 
     pf = problem.pareto_front()
     if pf is not None:
