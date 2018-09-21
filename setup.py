7f94d66cf43e2d2c08c7238917c9aaeff9a7e016
Julian <jules89@arcor.de>
2018-09-21 10:28:05 -0400

    VERSION 0.2.1

--- setup.py
+++ setup.py
@@ -3,29 +3,29 @@ import setuptools
 from Cython.Build import cythonize
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+__name__ = "pymoo"
 __author__ = "Julian Blank"
-__version__ = '0.2.1-dev8'
+__version__ = '0.2.1'
 __url__ = "https://github.com/msu-coinlab/pymoo"
 
-try:
-    import pypandoc
 
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    long_description = ''
 
 setup(
-    name="pymoo",
+    name=__name__,
     version=__version__,
     author=__author__,
+    url=__url__,
     python_requires='>3.3.0',
     author_email="blankjul@egr.msu.edu",
     description="Multi-Objective Optimization Algorithms",
-    long_description=long_description,
-    url=__url__,
+    long_description=readme(),
     license='Apache License 2.0',
     keywords="optimization",
-    packages=setuptools.find_packages(include="*.pyx", exclude=["tests", "docs"]),
+    packages=setuptools.find_packages(exclude=['tests', 'docs']),
     setup_requires=['cython'],
     install_requires=['pymop==0.2.1', 'numpy', 'scipy', 'matplotlib'],
     ext_modules=cythonize(
