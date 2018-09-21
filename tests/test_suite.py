3015ef67aee56e65ebb173d6e3a26714c898f53c
Julian <jules89@arcor.de>
2018-09-21 10:28:05 -0400

    VERSION 0.2.1

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -2,17 +2,19 @@ import os
 import sys
 import unittest
 
-# add the path to be execute in the main directory
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+if __name__ == "__main__":
 
-testmodules = [
-    'tests.test_nsga2',
-    'tests.test_usage'
-]
+    # add the path to be execute in the main directory
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
-suite = unittest.TestSuite()
+    testmodules = [
+        'tests.test_nsga2',
+        'tests.test_usage'
+    ]
 
-for t in testmodules:
-    suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+    suite = unittest.TestSuite()
 
-unittest.TextTestRunner().run(suite)
+    for t in testmodules:
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+    unittest.TextTestRunner().run(suite)
