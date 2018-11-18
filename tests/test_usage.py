a426159824ec482ff93f8bf66ccfd1c47a73e4df
Julian <jules89@arcor.de>
2018-11-18 14:06:58 -0500

    Bugfix test_usage.py

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -9,22 +9,29 @@ class UsageTest(unittest.TestCase):
         USAGE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "pymoo", "usage")
         # USAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-        for fname in os.listdir(USAGE_DIR):
+        usages = os.listdir(USAGE_DIR)
+        print(usages)
+
+        for fname in usages:
 
             if fname == "test_usage.py":
                 continue
 
             with self.subTest(msg="Checking %s" % fname, fname=fname):
 
+                print("=" * 50)
+                print(fname)
+                print("=" * 50)
+
                 with open(os.path.join(USAGE_DIR, fname)) as f:
                     s = f.read()
 
                     no_plots = "import matplotlib\n" \
-                               "from matplotlib import pyplot as plt\n" \
-                               "plt.ioff()\n" \
                                "matplotlib.use('Agg')\n"
 
                     s = no_plots + s
+
+                    s += "\nmatplotlib.pyplot.close()\n"
 
                     try:
                         exec(s, globals())
