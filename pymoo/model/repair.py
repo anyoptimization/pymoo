0d9cff00bcbdb2e4a6ce9c9d81d8a1d3ee57e715
Julian <jules89@arcor.de>
2019-04-29 14:04:36 -0400

    VERSION 3.0.1.dev

--- pymoo/model/repair.py
+++ pymoo/model/repair.py
@@ -6,9 +6,9 @@ class Repair:
     This class is allows to repair individuals after crossover if necessary.
     """
 
-    def do(self, pop, problem, **kwargs):
-        return self._do(pop, problem, **kwargs)
+    def do(self, problem, pop, **kwargs):
+        return self._do(problem, pop, **kwargs)
 
     @abstractmethod
-    def _do(self, pop, problem, **kwargs):
+    def _do(self, problem, pop, **kwargs):
         pass
