165ebdb5783c38735deb0d7ccd2626581a1e0b86
Yash Vesikar <yash@Yashs-MacBook-Pro.local>
2019-11-21 16:14:22 -0500

    Added pf files for WFG1-9 generated based on the original C implementation. Correctness ensured for WFG 1-4 based on the test_wfg.py file. Creating practical Clutch problem as well however, this implementation is still incomplete. wfg.py evaluate methods are based on approaches implemented within DEAP but have been vectorized to leverage numpy arrays and pymoo structure.

---
+++ tests/problems/test_wfg.py
@@ -0,0 +1,67 @@
+import unittest
+
+import autograd.numpy as anp
+
+from pymoo.factory import get_problem
+import os
+
+
+def load(name):
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "resources")
+
+    X = anp.loadtxt(os.path.join(path, "%s.x" % name))
+
+    try:
+        F = anp.loadtxt(os.path.join(path, "%s.f" % name))
+
+        CV = None
+        if os.path.exists(os.path.join(path, "%s.cv" % name)):
+            CV = anp.loadtxt(os.path.join(path, "%s.cv" % name))
+
+    except:
+        return X, None, None
+
+    return X, F, CV
+
+# Uncomment to check WFG problems
+problems = [
+    ('WFG1', [2,4,2]),
+    ('WFG2', [2,4,2]),
+    ('WFG3', [2,4,2]),
+    ('WFG4', [2,4,2]),
+    ('WFG5', [2,4,2]),
+    ('WFG6', [2,4,2]),
+    ('WFG7', [2,4,2]),
+    ('WFG8', [2,4,2]),
+    ('WFG9', [2,4,2])
+]
+
+
+
+class CorrectnessTest(unittest.TestCase):
+
+    def test_problems(self):
+        for entry in problems:
+            name, params = entry
+            print("Testing: " + name)
+
+            X, F, CV = load(name)
+
+            if F is None:
+                print("Warning: No correctness check for %s" % name)
+                continue
+
+            problem = get_problem(name, *params)
+            _F, _G, _CV = problem.evaluate(X, return_values_of=["F", "G", "CV"])
+
+            if problem.n_obj == 1:
+                F = F[:, None]
+
+            self.assertTrue(anp.all(anp.abs(_F - F) < 0.00001))
+
+            if problem.n_constr > 0:
+                self.assertTrue(anp.all(anp.abs(_CV[:, 0] - CV) < 0.0001))
+
+
+if __name__ == '__main__':
+    unittest.main()
