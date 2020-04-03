422389a08c3ce7b9d1e59e5f0d6b09f7bf89358b
Julian Blank <blankjul@egr.msu.edu>
2020-04-03 10:44:08 -0400

    Fix unit test for visualization usages

--- tests/visualization/test_visualization.py
+++ tests/visualization/test_visualization.py
@@ -9,7 +9,7 @@ class VisualizationTest(unittest.TestCase):
 
     def test_usages(self):
         folder = os.path.join(get_pymoo(), "pymoo", "usage", "visualization")
-        test_usage([os.path.join(folder, fname) for fname in os.listdir(folder)])
+        test_usage([os.path.join(folder, fname) for fname in os.listdir(folder) if fname.endswith("py")])
 
 if __name__ == '__main__':
     unittest.main()
