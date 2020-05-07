69992dc65f4b5036c41fb01991c794530e4fea0b
Cyril Picard <cyril.picard@epfl.ch>
2020-05-07 12:12:07 +0200

    Add MW constrained problems

--- pymoo/problems/multi/__init__.py
+++ pymoo/problems/multi/__init__.py
@@ -2,6 +2,7 @@ from pymoo.problems.multi.bnh import *
 from pymoo.problems.multi.carside import *
 from pymoo.problems.multi.ctp import *
 from pymoo.problems.multi.kursawe import *
+from pymoo.problems.multi.mw import *
 from pymoo.problems.multi.osy import *
 from pymoo.problems.multi.tnk import *
 from pymoo.problems.multi.truss2d import *
