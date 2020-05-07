8c257340ffdf11bd8ad32b7c6186b09824df7f6a
Julian Blank <blankjul@egr.msu.edu>
2020-05-07 12:05:20 -0400

    DASC-MOP Tests - Different Difficulties

--- tests/problems/test_problems_dascomp.py
+++ tests/problems/test_problems_dascomp.py
@@ -6,21 +6,24 @@ import numpy as np
 from pymoo.factory import get_problem
 
 
-def load(name):
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "resources", "DASCOMP")
+def load(name, diff):
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "resources", "DASCOMP", str(diff))
     X = np.loadtxt(os.path.join(path, "%s.x" % name))
     F = np.loadtxt(os.path.join(path, "%s.f" % name))
     CV = np.loadtxt(os.path.join(path, "%s.cv" % name))[:, None]
     return X, F, -CV
 
 
-
-
 class CorrectnessTest(unittest.TestCase):
 
     def test_problems(self):
 
-        for diff_factors in [[0.25, 0.0, 0.0]]:
+        difficulties = [[0.25, 0.0, 0.0], [0.0, 0.25, 0.0], [0.0, 0.0, 0.25], [0.25, 0.25, 0.25],
+                        [0.5, 0.0, 0.0], [0.0, 0.5, 0.0], [0.0, 0.0, 0.5], [0.5, 0.5, 0.5],
+                        [0.75, 0.0, 0.0], [0.0, 0.75, 0.0], [0.0, 0.0, 0.75], [0.75, 0.75, 0.75],
+                        [0.0, 1.0, 0.0], [0.5, 1.0, 0.0], [0.0, 1.0, 0.5], [0.5, 1.0, 0.5]]
+
+        for k, diff_factors in enumerate(difficulties):
 
             problems = [
                 get_problem("dascmop1", diff_factors),
@@ -36,17 +39,14 @@ class CorrectnessTest(unittest.TestCase):
 
             for problem in problems:
                 name = str(problem.__class__.__name__)
-                print("Testing: " + name)
+                print("Testing: " + name, "diff:", k)
 
-                X, F, CV = load(name)
+                X, F, CV = load(name, k)
                 _F, _CV = problem.evaluate(X, return_values_of=["F", "CV"])
 
                 np.testing.assert_allclose(_F, F)
                 np.testing.assert_allclose(_CV, CV)
 
 
-
 if __name__ == '__main__':
     unittest.main()
-
-
