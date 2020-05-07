42a122fff4a506830f88b705897013c8b593c033
Julian Blank <blankjul@egr.msu.edu>
2020-05-07 10:45:24 -0400

    DASCMOP Test Cases

--- pymoo/usage/termination/usage_manual_running.py
+++ pymoo/usage/termination/usage_manual_running.py
@@ -4,11 +4,12 @@ from pymoo.optimize import minimize
 from pymoo.util.running_metric import RunningMetric
 
 problem = get_problem("zdt3")
-algorithm = NSGA2(pop_size=100, callback=RunningMetric(5))
+algorithm = NSGA2(pop_size=100, callback=RunningMetric(10))
 
 if False:
     minimize(problem,
              algorithm,
+             ('n_gen', 200),
              pf=False,
              seed=1,
              verbose=True)
