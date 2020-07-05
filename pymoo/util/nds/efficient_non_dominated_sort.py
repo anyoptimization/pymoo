267336328742f68b62c0ee73597c076aaabb6528
Peng-YM <1048217874pengym@gmail.com>
2020-07-05 15:18:03 +0800

    Implement the tree-based efficient non-dominated sorting

--- pymoo/util/nds/efficient_non_dominated_sort.py
+++ pymoo/util/nds/efficient_non_dominated_sort.py
@@ -1,12 +1,13 @@
-import numpy as np
 from math import floor
+
+import numpy as np
 
 from pymoo.util.dominator import Dominator
 
 
 def efficient_non_dominated_sort(F, strategy="sequential"):
     """
-    Efficient Non-dominated Sorting
+    Efficient Non-dominated Sorting (ENS)
     Parameters
     ----------
     F: numpy.ndarray
@@ -15,7 +16,7 @@ def efficient_non_dominated_sort(F, strategy="sequential"):
         search strategy, can be "sequential" or "binary".
     Returns
     -------
-    indices of the individuals in each front.
+        indices of the individuals in each front.
 
     References
     ----------
@@ -25,8 +26,8 @@ def efficient_non_dominated_sort(F, strategy="sequential"):
     """
     assert (strategy in ["sequential", 'binary']), "Invalid search strategy"
     N, M = F.shape
-    # sort F in a ascending order of the first objective value
-    indices = np.argsort(F[:, 0])
+    # sort the rows in F
+    indices = sort_rows(F)
     F = F[indices]
     # front ranks for each individual
     fronts = []  # front with sorted indices
@@ -89,13 +90,13 @@ def binary_search(F, i, fronts):
     num_found_fronts = len(fronts)
     k_min = 0  # the lower bound for checking
     k_max = num_found_fronts  # the upper bound for checking
-    k = floor((k_max + k_min) / 2 + 0.5)     # the front now checked
+    k = floor((k_max + k_min) / 2 + 0.5)  # the front now checked
     current = F[i]
     while True:
         if num_found_fronts == 0:
             return 0
         # solutions in the k-th front, examine in reverse order
-        fk_indices = fronts[k-1]
+        fk_indices = fronts[k - 1]
         solutions = F[fk_indices[::-1]]
         non_dominated = True
         for f in solutions:
@@ -118,3 +119,23 @@ def binary_search(F, i, fronts):
                 return num_found_fronts
             else:
                 k = floor((k_max + k_min) / 2 + 0.5)
+
+
+def sort_rows(array, order='asc'):
+    """
+    Sort the rows of an array in ascending order.
+    The algorithm will try to use the first column to sort the rows of the given array. If ties occur, it will use the
+    second column, and so on.
+    Parameters
+    ----------
+    array: numpy.ndarray
+        array to be sorted
+    order: str
+        sort order, can be 'asc' or 'desc'
+    Returns
+    -------
+    the indices of the rows in the sorted array.
+    """
+    assert (order in ['asc', 'desc']), "Invalid sort order!"
+    ix = np.lexsort(array.T[::-1])
+    return ix if order == 'asc' else ix[::-1]
--- tests/misc/test_non_dominated_sorting.py
+++ tests/misc/test_non_dominated_sorting.py
@@ -9,9 +9,10 @@ class FastNonDominatedSortTest(unittest.TestCase):
     def assertFrontEqual(self, fronts_a, fronts_b):
         self.assertEqual(len(fronts_a), len(fronts_b))
         for a, b in zip(fronts_a, fronts_b):
+            self.assertEqual(len(a), len(b))
             self.assertEqual(set(a), set(b))
 
-    def test_non_dominated_sorting(self):
+    def test_fast_non_dominated_sorting(self):
         F = np.random.random((100,2))
         fronts = load_function("fast_non_dominated_sort", _type="python")(F)
         _fronts = load_function("fast_non_dominated_sort", _type="cython")(F)
@@ -19,7 +20,9 @@ class FastNonDominatedSortTest(unittest.TestCase):
         self.assertFrontEqual(fronts, _fronts)
 
     def test_efficient_non_dominated_sort(self):
-        F = np.random.random((100, 2))
+        print("Testing ENS...")
+        F = np.ones((1000, 3))
+        F[:, 1:] = np.random.random((1000, 2))
         _fronts = load_function("fast_non_dominated_sort", _type="python")(F)
         fronts = load_function("efficient_non_dominated_sort", _type="python")(F)
 
@@ -27,6 +30,16 @@ class FastNonDominatedSortTest(unittest.TestCase):
 
         fronts = load_function("efficient_non_dominated_sort", _type="python")(F, strategy="binary")
         self.assertFrontEqual(_fronts, fronts)
+
+    def test_tree_based_non_dominated_sort(self):
+        print("Testing T-ENS...")
+        F = np.ones((1000, 3))
+        F[:, 1:] = np.random.random((1000, 2))
+        _fronts = load_function("fast_non_dominated_sort", _type="python")(F)
+
+        fronts = load_function("tree_based_non_dominated_sort", _type="python")(F)
+        self.assertFrontEqual(_fronts, fronts)
+
 
 if __name__ == '__main__':
     unittest.main()
