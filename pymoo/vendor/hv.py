21e9cc143d42b471157780bbbe7cff4407c3856b
Aviv Shamsian <Mista2311@gmail.com>
2020-09-17 14:47:59 +0300

    Update hv.py

--- pymoo/vendor/hv.py
+++ pymoo/vendor/hv.py
@@ -101,7 +101,7 @@ class HyperVolume:
             hvRecursive = self.hvRecursive
             p = sentinel
             q = p.prev[dimIndex]
-            while q.cargo != None:
+            while q.cargo is not None:
                 if q.ignore < dimIndex:
                     q.ignore = 0
                 q = q.prev[dimIndex]
