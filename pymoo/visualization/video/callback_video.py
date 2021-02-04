a33dfda43bdd0ba99af606b42a9d33ba0c7aabd3
Rohit Dwivedula <rohitdwivedula@gmail.com>
2021-02-04 11:54:29 +0530

    add: save running metric plots to file

--- pymoo/visualization/video/callback_video.py
+++ pymoo/visualization/video/callback_video.py
@@ -8,17 +8,22 @@ class AnimationCallback(Callback):
     def __init__(self,
                  do_show=False,
                  do_close=False,
+                 save_plots=None,
                  nth_gen=1,
                  fname=None,
                  dpi=None,
                  video=None,
-                 exception_if_not_applicable=True):
+                 exception_if_not_applicable=True
+                 ):
 
         super().__init__()
         self.nth_gen = nth_gen
         self.do_show = do_show
         self.do_close = do_close
         self.exception_if_not_applicable = exception_if_not_applicable
+        self.save_plots = save_plots
+        self.save_plot_number = 1
+        self.dpi = dpi
 
         self.video = video
         if self.video is None and fname is not None:
@@ -36,6 +41,16 @@ class AnimationCallback(Callback):
             try:
 
                 ret = self.do(algorithm.problem, algorithm, **kwargs)
+                plots = plt.get_fignums()
+
+                if self.save_plots is not None:
+                    if len(plots) != 0:
+                        plt.savefig(
+                            self.save_plots + "_" + str(self.save_plot_number) + ".png", 
+                            dpi=self.dpi, 
+                            bbox_inches='tight'
+                        )
+                        self.save_plot_number += 1
 
                 if self.do_show:
                     plt.show()
