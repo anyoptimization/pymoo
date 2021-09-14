aa0c49962570ad2e9066f3d74a7b6727a5af813d
Julian Blank <blankjul@egr.msu.edu>
2021-09-14 11:16:01 -0400

    PF for CTP and MODACT

--- pymoo/experimental/scratch.py
+++ pymoo/experimental/scratch.py
@@ -1,25 +1,5 @@
+from pymoo.factory import MODAct
+from pymoo.util.plotting import plot
 
-from unittest.mock import MagicMock
-
-from pymoo.algorithms.nsga2 import NSGA2
-
-from pymoo.factory import get_problem
-from pymoo.optimize import minimize
-from pymoo.util.optimum import filter_optimum
-
-problem = get_problem("zdt1")
-
-algorithm = NSGA2(pop_size=100)
-algorithm._set_optimum = MagicMock(return_value=None)
-
-res = minimize(problem,
-               algorithm,
-               ('n_gen', 200),
-               seed=1,
-               verbose=False)
-
-opt = filter_optimum(res.algorithm.pop)
-
-print(opt)
-
-
+problem = MODAct("cs3")
+plot(problem.pareto_front(), no_fill=True)
