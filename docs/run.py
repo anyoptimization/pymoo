866e06028ec0492a20ce39bf0805d8d825fc686f
Julian Blank <blankjul@msu.edu>
2022-07-29 11:51:17 -0700

    Clear ipynb files

--- docs/run.py
+++ docs/run.py
@@ -9,11 +9,20 @@ SKIP = ["parallelization.ipynb",
 
 IPYNBS = [e for e in files_from_folder(DOCS, regex='**/*.ipynb', skip=SKIP) if ".ipynb_checkpoints" not in e]
 
+
+failed = []
+
 for ipynb in IPYNBS:
     print(ipynb, end="")
     try:
         KERNEL = start_new_kernel(kernel_name='python3')
-        run_ipynb(KERNEL, ipynb, overwrite=True, remove_trailing_empty_cells=True)
+        run_ipynb(KERNEL, ipynb, overwrite=True, remove_trailing_empty_cells=True, verbose=False)
         print(" OK")
     except:
+        failed.append(ipynb)
         print(" FAIL")
+
+print("FAILED:", len(failed))
+
+for e in failed:
+    print(e)
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -218,7 +218,7 @@ Content
 Contact
 ********************************************************************************
 
-| `Julian Blank <http://julianblank.com>`_  (blankjul [at] egr.msu.edu)
+| `Julian Blank <http://julianblank.com>`_  (blankjul [at] msu.edu)
 | Michigan State University
 | Computational Optimization and Innovation Laboratory (COIN)
 | East Lansing, MI 48824, USA
