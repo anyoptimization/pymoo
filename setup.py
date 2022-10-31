cfba9bf877c6b3f59a3e719d1e2741560e343e86
mooscalia project <93492480+mooscaliaproject@users.noreply.github.com>
2022-10-31 20:15:53 -0300

    Fix setup args and sys argv

--- setup.py
+++ setup.py
@@ -82,12 +82,12 @@ data['long_description_content_type'] = 'text/x-rst'
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--nopyx', action='store_true', help='Whether the pyx files shall be considered at all.')
-parser.add_argument('--nocython', action='store_true', help='Whether pyx files shall be cythonized.')
-parser.add_argument('--nolibs', action='store_true', help='Whether the libraries should be compiled.')
+parser.add_argument('--nopyx', dest='nopyx', action='store_true', help='Whether the pyx files shall be considered at all.')
+parser.add_argument('--nocython', dest='nocython', action='store_true', help='Whether pyx files shall be cythonized.')
+parser.add_argument('--nolibs', dest='nolibs', action='store_true', help='Whether the libraries should be compiled.')
 args, _ = parser.parse_known_args()
 
-sys.argv = [e for e in sys.argv if not e in args]
+sys.argv = [e for e in sys.argv if not e.lstrip("-") in args]
 
 
 # ============================================================
