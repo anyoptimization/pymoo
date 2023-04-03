c6c7fac2ed6be3474cb19741820057055ea87d21
Jacob Schmieder <jaikinator@googlemail.com>
2023-04-03 03:35:39 +0200

    Joblib Parallelization (#382)
    * added Joblib Parallelization
    
    * updated JoblibParallelization

--- pymoo/core/problem.py
+++ pymoo/core/problem.py
@@ -58,6 +58,23 @@ class DaskParallelization:
         return state
 
 
+class JoblibParallelization:
+
+    def __init__(self, aJoblibParallel,  aJoblibDelayed, *args, **kwargs) -> None:
+        super().__init__()
+        self.parallel = aJoblibParallel 
+        self.delayed =  aJoblibDelayed 
+
+    def __call__(self, f, X):
+        return self.parallel(self.delayed(f)(x) for x in X)
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state.pop("parallel", None)
+        state.pop("delayed", None)
+        return state
+
+
 class Problem:
     def __init__(self,
                  n_var=-1,
