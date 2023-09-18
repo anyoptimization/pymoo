cb1a5ab0b6f92323e297e42a454d7ade0538ba95
CompRhys <rhys.goodall@outlook.com>
2023-09-18 10:44:25 -0700

    fea: updated to use __new__

--- pymoo/__init__.py
+++ pymoo/__init__.py
@@ -4,13 +4,15 @@ import threading
 
 # Create the global random state Singleton
 class PymooPRNG(object):
-   _lock = threading.Lock()
-   _instance = None
+    _lock = threading.Lock()
+    _instance = None
 
-   @classmethod
-   def instance(cls, seed=None):
-        if not cls._instance:
+    def __new__(cls, seed=None):
+        if cls._instance is None: 
             with cls._lock:
+                # Another thread could have created the instance
+                # before we acquired the lock. So check that the
+                # instance is still nonexistent.
                 if not cls._instance:
                     cls._instance = np.random.default_rng(seed)
                     
