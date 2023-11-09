c020ccc178f0013d0d994936870b30c6efb01361
Ian Kropp <iankropp@gmail.com>
2023-11-09 13:06:13 -0500

    Adding dummy functions that mock up the I/O of value functions

--- pymoo/util/value_functions.py
+++ pymoo/util/value_functions.py
@@ -1,4 +1,21 @@
+import numpy as np
 
+
+
+## General I/O: 
+
+# Input: Most abstract: user does pair wise-comparison of points
+
+#       In between: if no pair can be see as better than others, either the set of solutions is t
+
+
+# Input less abstract 1: A list of non-dominated points 
+# Input less abstract 2: The ranking of the given non-dominated points 
+
+
+def create_value_fnc(F, rankings): 
+
+    return lambda f_new:  np.sum(f_new)
 
 
 def test_sum(a, b):
--- tests/misc/test_value_functions.py
+++ tests/misc/test_value_functions.py
@@ -1,12 +1,24 @@
 import pytest
 
-from pymoo.util import value_functions
+from pymoo.util import value_functions as vf
+import numpy as np
 
+test_dummy_val_fnc_inputs = [
+    (np.array([[1,2], [2,3]]), [1,2], np.array([1,2]), 3),
+    (np.array([[2,3], [3,2]]), [2,1], np.array([3,2]), 5)
+]
 
+# This is a general test for function I/O. 
+# It should take in a set of PO points, and then return a function. 
+# That function should take in a given PO point and return the 
+# value of that point according to the decision maker
 
-@pytest.mark.parametrize('a,b,expect_sum', [(1, 2, 3), (2, 3, 5)])
-def test_sum(a, b, expect_sum):
-    assert value_functions.test_sum(a, b) == expect_sum
+@pytest.mark.parametrize('F, rankings, test_f, expected_value', test_dummy_val_fnc_inputs)
+def test_dummy_val_fnc(F, rankings, test_f, expected_value):
+
+    val_fnc = vf.create_value_fnc(F, rankings)
+
+    assert val_fnc(test_f) == expected_value
 
 
 
