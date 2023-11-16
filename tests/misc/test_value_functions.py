3803c94485937779fe236d706ed64ace9a240034
Ian Kropp <iankropp@gmail.com>
2023-11-16 13:43:19 -0500

    Now also testing whether the P in the result are sorted properly

--- tests/misc/test_value_functions.py
+++ tests/misc/test_value_functions.py
@@ -21,23 +21,32 @@ def test_dummy_vf(F, rankings, test_f, expected_value):
     assert val_fnc(test_f) == expected_value
 
 
-test_test_prob_const_inputs = [
-    (np.array([[1,2], [2,3], [4,5]]), [1,3,2]),
-    (np.array([[2,3], [3,2], [7,8]]), [5,2,1])
+test_test_prob_const_in_out = [
+    (np.array([[1,2], [2,3], [4,5]]), [1,3,2], np.array([[1,2], [4,5], [2,3]])),
+    (np.array([[2,3], [3,2], [7,8]]), [5,2,1], np.array([[7,8], [3,2], [2,3]]))
 ]
-@pytest.mark.parametrize('F, rankings', test_test_prob_const_inputs)
-def test_prob_const(F, rankings):
+
+
+
+@pytest.mark.parametrize('F, rankings, output', test_test_prob_const_in_out)
+def test_prob_const(F, rankings, output):
 
     pymoo_prob = vf.OptimizeVF(F, rankings)
 
-    # Test whether the solutions are ranked by 
+    ## Test whether the solutions are ranked by ranking 
     ranks_from_prob = pymoo_prob.P[:, -1]
+    F_from_prob = pymoo_prob.P[:, 0:-1]
 
     rankings.sort()
 
-    assert ranks_from_prob.tolist() == rankings
-    
+    # Check the solutions 
+    assert np.all(output == F_from_prob)
 
+    # Check the ranks
+    assert ranks_from_prob.tolist() == rankings
+   
+
+    
 
     
 
