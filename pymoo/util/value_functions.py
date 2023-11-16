780d709a0b7aa605e3b209888ca94d223582529e
Ian Kropp <iankropp@gmail.com>
2023-11-16 11:24:44 -0500

    Passing smoke test for problem constructor

--- pymoo/util/value_functions.py
+++ pymoo/util/value_functions.py
@@ -1,30 +1,82 @@
 import numpy as np
 
+from pymoo.core.problem import Problem
+import pymoo.algorithms.soo.nonconvex.ga
 
+# Notes: 
+# I'm using the suffix _vf to denote variables used in the value-function optimization 
+# This is to avoid confuse it with variables being optimized in the main function 
 
-## General I/O: 
+# Input 1: A list of non-dominated points 
+# Input 2: The ranking of the given non-dominated points 
+# Input 3: constraint function for optimizing the value func
+# Input 4: the skeleton utility function that we're trying to optimize
+def create_vf(F, ranks, cFunc, utilFunc): 
 
-# Input: Most abstract: user does pair wise-comparison of points
+    # Combine rankings and objective functions into one matrix 
+    # The first O columns are the objective values, and the O + 1 column is the ranking
 
-#       In between: if no pair can be see as better than others, either the set of solutions is t
+    X = np.hstack((F, np.array([ranks]).T))
 
-
-# Input less abstract 1: A list of non-dominated points 
-# Input less abstract 2: The ranking of the given non-dominated points 
-
-
-def create_value_fnc(F, rankings): 
 
     return lambda f_new:  np.sum(f_new)
 
 
-def test_sum(a, b):
-    return a + b 
+
+def create_linear_vf(F, ranks): 
+
+    return lambda f_new:  np.sum(f_new)
 
 
+def linear_vf(F, x_vf): 
+
+    return np.multiply(F, x_vf)
 
 
+#def linear_const(F, x_vf):
 
+class OptimizeVF(Problem): 
+
+
+    def __init__(self, F, ranks):
+       
+        # One var for each dimension of the object space, plus epsilon 
+        n_var_vf = np.size(F, 0) + 1
+
+        # it has one inequality constraints per dimension in F, and one equality 
+        n_ieq_c_vf = n_var_vf - 1
+       
+        xl_vf = [0.0] * n_var_vf 
+        xu_vf = [1.0] * n_var_vf
+        
+        # upper/lower bound on the epsilon variable is -1000/1000
+        xl_vf[-1] = -1000
+        xu_vf[-1] = 1000
+
+        # TODO start everything at 0.5
+
+        # Add the rankings onto our objectives 
+        self.P = np.hstack((F, np.array([ranks]).T))
+
+        super().__init__(n_var_vf, n_obj=1, n_ieq_constr=n_ieq_c_vf, n_eq_constr=1, xl=xl_vf, xu=xu_vf)
+
+       
+
+    def _evaluate(self, x, out, *args, **kwargs):
+
+        ## Objective function: 
+
+        # maximize epsilon, or the minimum distance between each contour 
+        out["F"] = -x[:,-1]
+
+        ## Inequality
+        # TODO for now, assuming there are no ties in the ranks
+        out["G"] = -99
+        
+            
+        ## Equality
+        
+        out["H"] = -99
 
 
 
--- tests/misc/test_value_functions.py
+++ tests/misc/test_value_functions.py
@@ -14,12 +14,26 @@ test_dummy_val_fnc_inputs = [
 # value of that point according to the decision maker
 
 @pytest.mark.parametrize('F, rankings, test_f, expected_value', test_dummy_val_fnc_inputs)
-def test_dummy_val_fnc(F, rankings, test_f, expected_value):
+def test_dummy_vf(F, rankings, test_f, expected_value):
 
-    val_fnc = vf.create_value_fnc(F, rankings)
+    val_fnc = vf.create_linear_vf(F, rankings)
 
     assert val_fnc(test_f) == expected_value
 
 
+test_test_prob_const_inputs = [
+    (np.array([[1,2], [2,3]]), [1,2]),
+    (np.array([[2,3], [3,2]]), [2,1])
+]
+@pytest.mark.parametrize('F, rankings', test_test_prob_const_inputs)
+def test_prob_const(F, rankings):
+
+    val_fnc = vf.OptimizeVF(F, rankings)
+
+
+
+
+
+    
 
 
