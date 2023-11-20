ea56941ed3a1be62c95d02a5aab1a6fa59e3bed9
David Domínguez Barbero <dominguezbarradavid33@gmail.com>
2023-11-20 01:13:33 +0100

    Remove override termination for GA class

--- pymoo/algorithms/soo/nonconvex/ga.py
+++ pymoo/algorithms/soo/nonconvex/ga.py
@@ -77,8 +77,6 @@ class GA(GeneticAlgorithm):
                          output=output,
                          **kwargs)
 
-        self.termination = DefaultSingleObjectiveTermination()
-
 
 class BGA(GA):
 
