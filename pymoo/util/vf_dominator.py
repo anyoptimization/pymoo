f9a2368056d46890a8d63aa8ebf86a5535c11674
Ian Kropp <iankropp@gmail.com>
2024-07-02 15:27:39 -0400

    Fixing a bug with the value function dominance

--- pymoo/util/vf_dominator.py
+++ pymoo/util/vf_dominator.py
@@ -35,6 +35,7 @@ class VFDominator:
 
     @staticmethod
     def calc_domination_matrix_loop(F, G):
+
         n = F.shape[0]
         CV = np.sum(G * (G > 0).astype(float), axis=1)
         M = np.zeros((n, n))
@@ -65,19 +66,26 @@ class VFDominator:
 
         if self.algorithm.vf_res is not None: 
 
+            # Figure out what the v2 value is 
             v2 = self.algorithm.v2
 
+            # Get the value function
             vf = self.algorithm.vf_res.vf
 
-            F_vf = vf(F)[:,np.newaxis]
-            _F_vf = vf(_F)[:,np.newaxis]
+            # How much does the DM value each solution?
+            F_vf = vf(F * -1)[:,np.newaxis]
+            _F_vf = vf(_F * -1)[:,np.newaxis]
 
+            # We want to compare each solution to the others 
             Lv = np.repeat(F_vf, m, axis=0)
             Rv = np.tile(_F_vf, (n, 1))
-             
+            
+            # Which values are greater than (better) V2? 
             gtv2 = np.reshape(Lv < v2, (n, m))
+            # Which values are less than (worst) V2? 
             ltv2 = np.reshape(Rv > v2, (n, m))
-          
+         
+            # If you are greater than V2, you dominate all those who are smaller than V2
             split_by_v2 = np.logical_and(gtv2, ltv2)
            
             dom = np.logical_or(dom, split_by_v2)
@@ -85,7 +93,6 @@ class VFDominator:
         M = non_dom * 1 \
             + dom * -1
        
-
         return M
 
 
