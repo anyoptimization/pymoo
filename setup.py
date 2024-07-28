e5894d6fa39cdd3765c626d97c8e78f9ec856426
Julian Blank <blankjul@msu.edu>
2024-07-28 16:39:16 -0700

    Modify Deploy Script (1)

--- setup.py
+++ setup.py
@@ -49,11 +49,10 @@ data = dict(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Scientific/Engineering :: Mathematics'
