5e2298bd012bdddf2a450f55602fa35af4fb1e57
Tom Gillam <tpgillam@googlemail.com>
2025-04-12 18:01:54 +0100

    update for src layout

--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@ from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules=cythonize("pymoo/cython/*.pyx"), include_dirs=[numpy.get_include()])
+setup(
+    ext_modules=cythonize("src/pymoo/cython/*.pyx"), include_dirs=[numpy.get_include()]
+)
