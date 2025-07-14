21d3fdc515ceda6259c496fcdc9d46952f3eebdc
Gideon Oludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2025-07-14 13:05:06 -0400

    fix issue #740

--- pymoo/operators/crossover/nox.py
+++ pymoo/operators/crossover/nox.py
@@ -3,9 +3,8 @@ from pymoo.core.population import Population
 
 
 class NoCrossover(Crossover):
-
-    def __init__(self):
-        super().__init__(1, 1, 0.0)
+    def __init__(self, *, n_parents=1, n_offsprings=1, prob=0.0, **kwargs):
+        super().__init__(n_parents, n_offsprings, prob, **kwargs)
 
     def do(self, problem, pop, *args, random_state, **kwargs):
         return Population.create(*[random_state.choice(parents) for parents in pop])
