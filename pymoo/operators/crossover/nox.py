ab1dc5b7f73b4421c1fd7c2349f61c891643b7b4
Gideon Oludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2025-08-03 18:22:03 -0400

    enforce consistent number of parents and offsprings in NoCrossover

--- pymoo/operators/crossover/nox.py
+++ pymoo/operators/crossover/nox.py
@@ -5,8 +5,8 @@ from pymoo.core.population import Population
 
 
 class NoCrossover(Crossover):
-    def __init__(self, *, n_parents=1, n_offsprings=1, prob=0.0, **kwargs):
-        super().__init__(n_parents, n_offsprings, prob, **kwargs)
+    def __init__(self, *, n_parents=1, prob=0.0, **kwargs):
+        super().__init__(n_parents, n_parents, prob, **kwargs)
 
     def do(self, problem, pop, *args, random_state, **kwargs):
         return Population.create(*itertools.chain.from_iterable(pop))
