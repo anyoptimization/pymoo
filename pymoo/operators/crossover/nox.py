425d8d97b2b9e673bf3808d005aa5d06f9c4e543
Julian Blank <blankjul@outlook.com>
2025-11-16 11:18:20 -0800

    fix NoCrossover missing offsprings #744

--- pymoo/operators/crossover/nox.py
+++ pymoo/operators/crossover/nox.py
@@ -3,8 +3,11 @@ from pymoo.core.population import Population
 
 
 class NoCrossover(Crossover):
-    def __init__(self, *, n_parents=1, n_offsprings=1, prob=0.0, **kwargs):
+    def __init__(self, *, n_parents=1, n_offsprings=None, prob=0.0, **kwargs):
+        if n_offsprings is None:
+            n_offsprings = n_parents
         super().__init__(n_parents, n_offsprings, prob, **kwargs)
 
     def do(self, problem, pop, *args, random_state, **kwargs):
-        return Population.create(*[random_state.choice(parents) for parents in pop])
+        replace = self.n_offsprings > self.n_parents
+        return Population.create(*[random_state.choice(parents, replace=replace) for parents in pop])
